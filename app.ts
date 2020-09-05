ad6a8cf7759738ab16274ef43db494979fc3e95e
mdrijwan <mrrmmonty@gmail.com>
2020-09-05 20:29:13 +0800

    Taking user input to be converted

--- app.ts
+++ app.ts
@@ -1,26 +1,42 @@
 import express from "express";
 import api from "./config/config";
+import bodyParser from "body-parser";
+
 
 const request = require("request");
 const serverless = require ("serverless-http");
 
 let app = express();
 
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/convert', (req, res) => {
     const fromCurrency = req.query.fr
     const toCurrency = req.query.to
-    let qs = fromCurrency + '_' + toCurrency;
+    let amount:any = req.query.amount
+    let q = fromCurrency + '_' + toCurrency;
+    console.log('q:', q)
+    console.log('amount:', amount)
   
-    let url = api.url + 'convert?q=' + qs + '&compact=ultra&apiKey=' + api.apiKey;
+    let url = api.url + 'convert?q=' + q + '&compact=ultra&apiKey=' + api.apiKey;
 
     request(url, function (error, response, body) {
       console.log('error:', error);
       console.log('statusCode:', response && response.statusCode);
       console.log('body:', body);
-      res.send(JSON.parse(body))
+      
+      let jsonObj = JSON.parse(body);
+      let val = (jsonObj[Object.keys(jsonObj)[0]]);      
+      let finalAmount:number = parseFloat(amount) * val
+      console.log('finalAmount:', finalAmount)
+      
+      const result = {
+          "Conversion Rate": JSON.parse(body),
+          "Given from": `${amount} ${fromCurrency}`,
+          "Converted to": `${finalAmount} ${toCurrency}`
+      };
+      res.send(result)
     })
 });
 
